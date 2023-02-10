ac1799d5e0ce7dc721acf6bcb0cece676aa1f056
Adel Tahri <adel4tahri@gmail.com>
2023-02-10 20:50:52 +0100

    🐛 FIX:

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
+const affectedProps = require("./affected-props");
+const {validateOptions} = require('./options');
 /**
  * @type {import('postcss').PluginCreator}
  */
-module.exports = (opts = {}) => {
+const plugin = (options) =>  {
   // Work with options here
+  options = validateOptions(options);
+  const whitelist = new Set(options.whitelist);
+  const blacklist = new Set(options.blacklist);
+
 
   return {
-    postcssPlugin: 'postcss-dima-rtl',
+    postcssPlugin: "postcss-dima-rtl",
     Once(root) {
       root.walkRules((rule) => {
         rule.walkDecls((decl) => {
-          if (decl.prop === 'text-align-start') {
-            // Check if the file name ends with "-rtl"
-            if (rule.parent.source.input.file.endsWith('-rtl.css')) {
-              decl.prop = 'text-align';
-              decl.value = 'right';
-            } else {
-              decl.prop = 'text-align';
-              decl.value = 'left';
+          if (rule.parent.source.input.file.endsWith("-rtl.css")) {
+            if (affectedProps.indexOf(decl.prop) >= 0) {
+              dirDecls.push(decl);
+
+              if (decl.prop === "text-align") {
+                decl.prop = "text-align";
+                decl.value = "right";
+              }
             }
           }
         });
-      });  
-    }
-  }
-}
+      });
+    },
+  };
+};
 
-module.exports.postcss = true
+
+plugin.postcss = true;
+
+module.exports = plugin;
