1f1520cfc167cffcf1ede657515cbd0ce1798628
Adel Tahri <adel4tahri@gmail.com>
2023-02-10 21:40:04 +0100

    🐛 FIX: save

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
 const affectedProps = require("./affected-props");
-const {validateOptions} = require('./options');
+const { validateOptions } = require("./options");
 /**
  * @type {import('postcss').PluginCreator}
  */
-const plugin = (options) =>  {
+const plugin = (options) => {
   // Work with options here
   options = validateOptions(options);
   const whitelist = new Set(options.whitelist);
   const blacklist = new Set(options.blacklist);
 
+  /**
+   * Description: Check if the property is allowed by the whitelist and blacklist options.
+   * @param {*} prop
+   * @returns
+   */
+  const isAllowedProp = (prop) => {
+    const isAllowedByWhitelist = !options.whitelist || whitelist.has(prop);
+    const isAllowedByBlacklist = !options.blacklist || !blacklist.has(prop);
+    return isAllowedByWhitelist && isAllowedByBlacklist;
+  };
 
   return {
     postcssPlugin: "postcss-dima-rtl",
-    Once(root) {
-      root.walkRules((rule) => {
-        rule.walkDecls((decl) => {
-          if (rule.parent.source.input.file.endsWith("-rtl.css")) {
-            if (affectedProps.indexOf(decl.prop) >= 0) {
-              dirDecls.push(decl);
 
-              if (decl.prop === "text-align") {
-                decl.prop = "text-align";
-                decl.value = "right";
-              }
+    Once(root) {
+      const dirDecls = [];
+
+      root.walkRules((rule) => {
+        if (!rule.parent.source.input.file.endsWith("-rtl.css")) return;
+
+        rule.walkDecls((decl) => {
+          if (!isAllowedProp(decl.prop)) return;
+
+          if (affectedProps.indexOf(decl.prop) >= 0) {
+            dirDecls.push(decl);
+
+            if (decl.prop === "text-align") {
+              decl.prop = "text-align";
+              decl.value = "right";
             }
           }
         });
+
+        if (dirDecls.length) {
+          getDirRule(rule, 'dir', options).append(dirDecls);
+        }
       });
     },
   };
 };
-
 
 plugin.postcss = true;
 
